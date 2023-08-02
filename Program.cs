330e1915af125eda9a50563ab05a8633eaabfbc4
NhonVo <vothuongtruongnhon2002@gmail.com>
2023-08-02 22:31:35 +0700

    cors

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(p => p.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseHttpsRedirection();
 
